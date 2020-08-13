d491ac2f12807488ea5c67660f9ca496b7cd5061
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-08-13 11:02:30 +0200

    events section  added

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,6 +1,7 @@
 // React & Tools
 import React, { useState, useEffect, useContext } from 'react';
-//import { useMediaQuery } from 'react-responsive';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 // Components
 import Heading from './static/Heading';
@@ -14,34 +15,91 @@ import { AssetsContext } from './static/Assets';
 const url = `http://localhost:4000/events`;
 
 const Events = () => {
-	const [data, setData] = useState('');
-	const assets = useContext(AssetsContext);
+	// Date & conversion
+	const [eventDates, setEventDates] = useState([]);
+	const months = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'Maj',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Okt',
+		'Nov',
+		'Dec',
+	];
+
 	// Fetch
+	const [data, setData] = useState();
+	const assets = useContext(AssetsContext);
 	useEffect(() => {
 		(async () => {
 			try {
 				const response = await fetch(url);
 				const result = await response.json();
-				console.log(result);
 				setData(result);
+
+				result.forEach((e) => {
+					console.log(e.eventDate);
+					let date = new Date(e.eventDate);
+					let day = date.getDate();
+					let month = date.getMonth();
+					let hour = date.getHours();
+					let minute = date.getMinutes();
+					setEventDates((eventDates) => [
+						...eventDates,
+						{ day, month: months[month], hour, minute },
+					]);
+				});
 			} catch (e) {
 				console.log('Error:', e);
 			}
 		})();
+		// eslint-disable-next-line
 	}, []);
+
+	let srcArr = [];
+	for (let i = 4; i < 10; i++) {
+		srcArr.push(i);
+	}
+
 	return (
 		<section className='events'>
 			<Heading white={'events of the'} linebr={'month'} />
-			<div className='event__container'>
-				{assets === undefined ? '' : console.log(assets)}
+			<div className='events__container'>
+				{/* assets === undefined ? '' : console.log(assets) */}
 				{assets === undefined ? (
 					<p>Loading...</p>
 				) : (
-					<img
-						className='events__img'
-						src={assets[0].url}
-						alt={data[0].eventName}
-					/>
+					<Carousel className='events__carousel'>
+						{srcArr.map((src, index) => (
+							<div key={index}>
+								<img
+									src={assets[src].url}
+									alt={data[index].eventName}
+								/>
+								<div className='events__infoContainer'>
+									<p className='events__info'>
+										{eventDates[index].day}{' '}
+										{eventDates[index].month}
+									</p>
+									<p className='events__info'>
+										{eventDates[index].hour}
+										{':'}
+										{eventDates[index].minute < 10
+											? `0${eventDates[index].minute}`
+											: eventDates[index].minute}
+									</p>
+									<p className='events__info'>
+										{data[index].location}
+									</p>
+								</div>
+							</div>
+						))}
+					</Carousel>
 				)}
 			</div>
 		</section>
